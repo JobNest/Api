23f15199c8ddc2b00aec5cb4620cfe4bee9b48a2
guodongxiaren <879231132@qq.com>
2019-05-11 13:16:23 +0800

    from app import app failed

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@ PYTHON = 'python3'
 all: 
 	$(PYTHON) run.py
 
+online: 
+	nohup $(PYTHON) run.py &
+
 .PHONY: clean
 clean:
 	rm *.pyc *.txt
--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
-from app import search
-from app import login
 import yaml
 from flask import Flask
 
 app = Flask(__name__)
 conf = yaml.load(open('./conf/global.yml'))
 app.config.update(conf)
+
+from app import search
+from app import login
